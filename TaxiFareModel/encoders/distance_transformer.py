620d7ad39ea818091ac6f913316dda6fbaecc8e4
Brahim El Merbouh <elmerbouh@gmail.com>
2022-02-03 20:49:19 -0500

    refactoring

---
+++ TaxiFareModel/encoders/distance_transformer.py
@@ -0,0 +1,30 @@
+from sklearn.base import BaseEstimator, TransformerMixin
+import pandas as pd
+from TaxiFareModel.utils import haversine_vectorized
+
+class DistanceTransformer(BaseEstimator, TransformerMixin):
+    """Compute the haversine distance between two GPS points."""
+    def __init__(self,
+                 start_lat="pickup_latitude",
+                 start_lon="pickup_longitude",
+                 end_lat="dropoff_latitude",
+                 end_lon="dropoff_longitude"):
+        self.start_lat = start_lat
+        self.start_lon = start_lon
+        self.end_lat = end_lat
+        self.end_lon = end_lon
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y=None):
+        assert isinstance(X, pd.DataFrame)
+        X_ = X.copy()
+        X_["distance"] = haversine_vectorized(
+            X_,
+            start_lat=self.start_lat,
+            start_lon=self.start_lon,
+            end_lat=self.end_lat,
+            end_lon=self.end_lon
+        )
+        return X_[['distance']]
---
+++ TaxiFareModel/encoders/time_features_encoder.py
@@ -0,0 +1,21 @@
+from sklearn.base import BaseEstimator, TransformerMixin
+import pandas as pd
+
+class TimeFeaturesEncoder(BaseEstimator, TransformerMixin):
+    def __init__(self, time_column, time_zone_name='America/New_York'):
+        self.time_column = time_column
+        self.time_zone_name = time_zone_name
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y=None):
+        assert isinstance(X, pd.DataFrame)
+        X_ = X.copy()
+        X_.index = pd.to_datetime(X[self.time_column])
+        X_.index = X_.index.tz_convert(self.time_zone_name)
+        X_["dow"] = X_.index.weekday
+        X_["hour"] = X_.index.hour
+        X_["month"] = X_.index.month
+        X_["year"] = X_.index.year
+        return X_[['dow', 'hour', 'month', 'year']]
